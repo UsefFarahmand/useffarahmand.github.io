2728c4cfc532afb5fc0c0758264e14907cac6c1b
Usef Farahmand <50228817+UsefFarahmand@users.noreply.github.com>
2026-06-19 10:40:43 +0330

    Auto open dropdown menu when entering My Work section

--- js/scripts.js
+++ js/scripts.js
@@ -167,3 +167,37 @@ if(myWorkBtn){
     });
 
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const exploreSection = document.querySelector("#explore");
+    const navDropdown = document.querySelector(".nav-dropdown");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+
+            entries.forEach(entry => {
+
+                if (entry.isIntersecting) {
+
+                    // رسیدن به My Work
+                    navDropdown.classList.add("open");
+
+                } else {
+
+                    // خروج از My Work
+                    navDropdown.classList.remove("open");
+
+                }
+
+            });
+
+        },
+        {
+            threshold: 0.4
+        }
+    );
+
+    observer.observe(exploreSection);
+
+});
